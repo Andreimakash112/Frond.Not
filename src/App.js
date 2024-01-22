784b60d1c3ec576d0206743553eadb5cedf72772
Andrei.Makash <andrei.makash@yandex.ru>
2024-01-22 14:24:36 +0300

    прописаны стартовые стили .исправлены ошибки

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+     <Header />
+     <Main />
+     <Footer />
     </div>
   );
 }

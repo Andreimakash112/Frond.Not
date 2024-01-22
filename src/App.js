7844ec68e45e89232d278fdd598b8458b8f065c4
Andrei.Makash <andrei.makash@yandex.ru>
2024-01-22 13:49:15 +0300

    прописаны стартовые стили для шапки.тела.подвала.добавлены оные в App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Main />
+      <footer />
     </div>
   );
 }

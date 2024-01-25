18b44e0a513deb23fe2a82d69a277910ff6d0f1b
Andrei.Makash <andrei.makash@yandex.ru>
2024-01-25 20:06:12 +0300

    дописали во все файлы js.строку из index.js.(import React from 'react';)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './views/Main';
 import Footer from './components/Footer';
+import Basket from './views/Basket';
 function App() {
+
+  const[page, setPage] = useState('Main')
+
+  const pages = {
+    Main: <Main />,
+    Basket: <Basket />
+
+  }
+
   return (
     <div className="App">
-     <Header />
-     <Main />
+     <Header setPage={setPage} />
+     { pages[page] }
      <Footer />
     </div>
   );

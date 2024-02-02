7d43da26acbc6d7c9bb88019525c901e0bce74c5
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-02 14:07:26 +0300

    в app.js добавили import Login и в const modalBoxes строка Login и в конце перед /ModalBox добавили <Login />

--- src/components/ModalBox.js
+++ src/components/ModalBox.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './ModalBox.css';
 
-function ModalBox({setModalBox}) {
+function ModalBox({setModalBox, children}) {
   return (
 <>
     <div className="echo" onClick ={() =>setModalBox('none')}>
     </div>
     <div className="ModalBox">
 
-
+      { children}
 
     </div>
     

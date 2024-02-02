26179f998773bb8549c1b93971a094fd9cbf384b
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-02 20:23:02 +0300

    добавили в Login.js точка остановки кода( debugger console.log(data)  )

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,13 +10,15 @@ function Login() {
         login:login,
         password:password
     }
+    debugger
+    console.log(data) 
  }
   return ( 
     <>
       <h1>Авторизация</h1>
        <input id='login'type='text' placeholder='введите логин' />
        <input id='password' type='password' placeholder='введите пароль' />
-       <button>войти</button>
+       <button onClick={Log}>войти</button>
     </>
   );
 }

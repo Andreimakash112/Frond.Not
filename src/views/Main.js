ec358f8468b61d5aaa8e950f1e5297c560d8a6cb
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-06 13:40:50 +0300

    прописываем в main (const Products = [{ header: ,image: ,price}] сюда поступают данные с бд

--- src/views/Main.js
+++ src/views/Main.js
@@ -2,38 +2,19 @@ import React from 'react';
 import './Main.css';
 import Product from '../components/Product';
 import image from '../images/01.jpg'
-function Main() {
-const Products = [
-  {
-    header: 'Товар 1',
-    image: image,
-    price: 120
-  },
-  {
-    header: 'Товар 1',
-    image: image,
-    price: 120
-  },
-  {
-    header: 'Товар 1',
-    image: image,
-    price: 120
-  },
-  {
-    header: 'Товар 1',
-    image: image,
-    price: 120
-  },
-  {
-    header: 'Товар 1',
-    image: image,
-    price: 120
-  },
-  {
-    header: 'Товар 1',
-    image: image,
-    price: 120
-  }
+function Main() { 
+  const products = [
+  {header: 'Товар 1',image: image,price: 12000 }, 
+  {header: 'Товар 1',image: image, price: 14000 },
+  
+  {header: 'Товар 1',image: image,price: 16000 },
+    
+  {header: 'Товар 1',image: image, price: 18000 },
+    
+  {header: 'Товар 1',image: image,price: 20000 },
+    
+  {header: 'Товар 1',image: image, price: 22000 }
+  
 ]
 
 

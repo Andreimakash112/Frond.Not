c1360842e47186c2fd5abaff957236c12761a5b6
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-06 17:42:14 +0300

    добавили стилей

--- src/views/Main.js
+++ src/views/Main.js
@@ -7,6 +7,7 @@ import image_3 from '../images/03.jpg'
 import image_4 from '../images/04.jpg'
 import image_5 from '../images/05.jpg'
 import image_6 from '../images/06.jpg'
+import image_7 from '../images/07.jpg'
 
 function Main() { 
 
@@ -17,8 +18,9 @@ function Main() {
   {id:3,  header: 'Товар 3',image: image_3, price: 16000 },  
   {id:4,  header: 'Товар 4',image: image_4, price: 18000 }, 
   {id:5,  header: 'Товар 5',image: image_5, price: 20000 }, 
-  {id:6,  header: 'Товар 6',image: image_6, price: 22000 } 
-
+  {id:6,  header: 'Товар 6',image: image_6, price: 22000 }, 
+  {id:7,  header: 'Товар 7',image: image_7, price: 25000 } 
+  
 ]
 function getProductList() {
 

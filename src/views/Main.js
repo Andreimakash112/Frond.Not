42211efac81b3016d4f9242208747119e4b2494d
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-06 15:01:53 +0300

    в файле Main,js Обязательно меняем !!!!!function getProductList() {return products.map((item) => <Product key = {item.id} header = {item.header} image = {item.image} price = {item.price} />   )}   ррисваиваем id в const  products

--- src/views/Main.js
+++ src/views/Main.js
@@ -6,17 +6,17 @@ function Main() {
 
   const products = [
 
-  {header: 'Товар 1',image: image, price: 12000 }, 
-  {header: 'Товар 2',image: image, price: 14000 },
-  {header: 'Товар 3',image: image, price: 16000 },  
-  {header: 'Товар 4',image: image, price: 18000 }, 
-  {header: 'Товар 5',image: image, price: 20000 }, 
-  {header: 'Товар 6',image: image, price: 22000 } 
+  {id:1,  header: 'Товар 1',image: image, price: 12000 }, 
+  {id:2,  header: 'Товар 2',image: image, price: 14000 },
+  {id:3,  header: 'Товар 3',image: image, price: 16000 },  
+  {id:4,  header: 'Товар 4',image: image, price: 18000 }, 
+  {id:5,  header: 'Товар 5',image: image, price: 20000 }, 
+  {id:6,  header: 'Товар 6',image: image, price: 22000 } 
 
 ]
 function getProductList() {
 
-  return products.map((item) => <Product header = {item.header} image = {item.image} price = {item.price} />   )
+  return products.map((item) => <Product key = {item.id} header = {item.header} image = {item.image} price = {item.price} />   )
 
 }
 

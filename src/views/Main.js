0f2f275c58d3dec1be16d3d42f3a7840a06e51b1
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-06 14:16:56 +0300

    создаем в main function getProductList() {return products.forEach((item) => <Product header = {item.header} image = {item.image} price = {item.price} />   )}

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,19 +3,22 @@ import './Main.css';
 import Product from '../components/Product';
 import image from '../images/01.jpg'
 function Main() { 
+
   const products = [
-  {header: 'Товар 1',image: image,price: 12000 }, 
-  {header: 'Товар 1',image: image, price: 14000 },
-  
-  {header: 'Товар 1',image: image,price: 16000 },
-    
-  {header: 'Товар 1',image: image, price: 18000 },
-    
-  {header: 'Товар 1',image: image,price: 20000 },
-    
-  {header: 'Товар 1',image: image, price: 22000 }
-  
+
+  {header: 'Товар 1',image: image, price: 12000 }, 
+  {header: 'Товар 2',image: image, price: 14000 },
+  {header: 'Товар 3',image: image, price: 16000 },  
+  {header: 'Товар 4',image: image, price: 18000 }, 
+  {header: 'Товар 5',image: image, price: 20000 }, 
+  {header: 'Товар 6',image: image, price: 22000 } 
+
 ]
+function getProductList() {
+
+  return products.forEach((item) => <Product header = {item.header} image = {item.image} price = {item.price} />   )
+
+}
 
 
   return (

c47848616d6b877699bc458599ea8c65ccbac77a
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-06 13:13:21 +0300

    прописываем в main (const Products = [{ header: ,image: ,price:}])

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,6 +3,15 @@ import './Main.css';
 import Product from '../components/Product';
 import image from '../images/01.jpg'
 function Main() {
+const Products = [
+  {
+    header: 'Товар 1',
+    image: image,
+    price: 120
+  },
+]
+
+
   return (
     <div className="Main">
      <Product image = {image} /> 

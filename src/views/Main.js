3f743f400bb5b4b1e31bde42d3527a393dadc3e7
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-06 12:31:13 +0300

    добавили папку images,картинки товара,в main делаем import image from ../images/01.jpg

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Main.css';
 import Product from '../components/Product';
+import image from '../images/01.jpg'
 function Main() {
   return (
     <div className="Main">

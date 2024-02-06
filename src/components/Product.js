272fdeb8a14121e7c3b2448f02f7306168bbb72f
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-06 12:53:22 +0300

    прописываем в product.js ,в function roduct({image})и меняем img src = {image}

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({image}) {
   return (
     <div className="Product">
-      <img src='' />
+      <img src = {image}/>
       <h1>Товар</h1>
       <p>{ `${120} руб` }</p>
       <button> В корзину </button>

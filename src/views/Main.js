a5e7c4b0b3f2651e7c0049e22b22fd08170cb3e5
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-07 16:15:58 +0300

    (frontend) в файле main удаляем товары из масива из[]

--- src/views/Main.js
+++ src/views/Main.js
@@ -11,17 +11,8 @@ import image_7 from '../images/07.jpg'
 
 function Main() { 
 
-  const products = [
+  const products = []
 
-  {id:1,  header: 'Товар 1',image: image, price: 12000 }, 
-  {id:2,  header: 'Товар 2',image: image_2, price: 14000 },
-  {id:3,  header: 'Товар 3',image: image_3, price: 16000 },  
-  {id:4,  header: 'Товар 4',image: image_4, price: 18000 }, 
-  {id:5,  header: 'Товар 5',image: image_5, price: 20000 }, 
-  {id:6,  header: 'Товар 6',image: image_6, price: 22000 }, 
-  {id:7,  header: 'Товар 7',image: image_7, price: 25000 } 
-  
-]
 function getProductList() {
 
   return products.map((item) => <Product key = {item.id} header = {item.header} image = {item.image} price = {item.price} />   )

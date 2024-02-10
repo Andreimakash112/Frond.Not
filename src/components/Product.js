c99478a837db6f77aedf77e265ea25dc04d85cfd
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-10 20:51:56 +0300

    (frontend)ощибки исправлены стартовый becend закончен все работает )

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ import './Product.css';
 function Product({header,image,price}) {
   return (
     <div className="Product">
-      <img  alt = "изображение товара"src = {image}/>
+      <img  alt = ""src = {image}/>
       <h1>{header}</h1>
       <p>{ `${price} руб` }</p>
       <button> В корзину </button>
